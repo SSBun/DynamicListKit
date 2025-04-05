ac4dc65ad610353491e0d4b14654846d2ffa26a0
caishilin <caishilin@yahoo.com>
2025-04-05 21:16:26 +0800

    Initial Commit

---
+++ InfinityListViewTests/InfinityListViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  InfinityListViewTests.swift
+//  InfinityListViewTests
+//
+//  Created by caishilin on 2025/4/5.
+//
+
+import Testing
+@testable import InfinityListView
+
+struct InfinityListViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
